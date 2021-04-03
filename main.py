2a5b0d6153e60e05279bf87f6c9edfc3a9e384d1
JakaPuric <jakapuric@gmail.com>
2021-04-03 19:29:38 +0200

    novo

--- main.py
+++ main.py
@@ -7,30 +7,48 @@
 
 #%%
 def izrisKlasicnihReber():
-#%%     
+    materiali = Q_f()[0]
+    Q_f_materiali = Q_f()[1]
     # importing modules
     import matplotlib.pyplot as plt
+    # izris rezultatov
+    plt.bar(materiali, Q_f_materiali, color ='maroon', width = 0.4)
+
+    plt.xlabel("Različni materiali")
+    plt.ylabel("Prenos toplote [W]")
+    plt.title("Prenos toplote v okolico skozi rebra")
+    plt.show()
+
+#%%
+def Q_f():
     from klasicnoRebro import izracunKlasicnegaRebra 
-    
+
     # definiranje materialov, ki nas zanimajo
     materiali = ['PMMA','ABS', 'aluminij', 'zelezo', 'baker']
     # izračun za vsak material
     Q_f_materiali = []
     for material in materiali:
-        Q_f_materiali.append(izracunKlasicnegaRebra(material))
-#%%
-    # izris rezultatov
-    plt.bar(materiali, Q_f_materiali, color ='maroon', width = 0.4)
-
-    plt.xlabel("Različni materiali")
-    plt.ylabel("Prenos toplote")
-    plt.title("Prenos toplote v okolico skozi rebra")
-    plt.show()
-
-
+        Q_f_materiali.append(izracunKlasicnegaRebra(material, 0.05, 0.005))
+    return materiali, Q_f_materiali
 
 #%%
 izrisKlasicnihReber()
+
+#%%
+Q_f()
+
+#%%
+#def Primerjava():
+Q_materiali = Q_f()[1]
+Q_max = max(Q_materiali)
+
+for 
+
+
+
+
+
+
 
 
 
