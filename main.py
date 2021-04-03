81e82e42afbc84b1c10a212f400b3e031809001f
JakaPuric <jakapuric@gmail.com>
2021-04-03 18:28:56 +0200

    Popravek + preizkus, test = dela k puska

--- main.py
+++ main.py
@@ -7,21 +7,21 @@
 
 #%%
 def izrisKlasicnihReber():
+#%%     
     # importing modules
     import matplotlib.pyplot as plt
     from klasicnoRebro import izracunKlasicnegaRebra 
-
-    # definiranje materialov, ki nas zanimajo
-    materiali = ['plastika','zelezo']
     
+    # definiranje materialov, ki nas zanimajo
+    materiali = ['PMMA','ABS', 'aluminij', 'zelezo', 'baker']
     # izračun za vsak material
-    q_materiali = []
+    Q_f_materiali = []
     for material in materiali:
-        q_materiali.append(izracunKlasicnegaRebra('plastika'))
-
+        Q_f_materiali.append(izracunKlasicnegaRebra(material))
+#%%
     # izris rezultatov
-    plt.bar(materiali, q_materiali, color ='maroon', width = 0.4)
- 
+    plt.bar(materiali, Q_f_materiali, color ='maroon', width = 0.4)
+
     plt.xlabel("Različni materiali")
     plt.ylabel("Prenos toplote")
     plt.title("Prenos toplote v okolico skozi rebra")
