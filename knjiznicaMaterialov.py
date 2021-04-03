6a0ed8a1ec4c7110de9cf0e9363f93e922061d20
JakaPuric <jakapuric@gmail.com>
2021-04-03 17:14:53 +0200

    Narejena knjiznica materialov

--- knjiznicaMaterialov.py
+++ knjiznicaMaterialov.py
@@ -1,25 +1,30 @@
 def knjiznica(material):
     materiali = {
-        'plastika': {
-            'lambda' : 1, #W/mK
-            'cp' : 1, #J/kgK
-            
+        'PMMA': {
+            'lambda': 0.193,  # W/mK
+            'cp': 1450,  # J/kgK
+
 
         },
+        'ABS': {
+            'lambda': 0.24,  # W/mK
+            'cp': 1675,  # J/kgK
+        },
+        'aluminij': {
+            'lambda': 240,  # W/mK
+            'cp': 910,  # J/kgK
+        },
         'zelezo': {
-            'lambda' : 1, #W/mK
-            'cp' : 1, #J/kgK
-        }
-
-        ,
-        'zelezo': {
-            'lambda' : 1, #W/mK
-            'cp' : 1, #J/kgK
+            'lambda': 65,  # W/mK
+            'cp': 450,  # J/kgK
+        },
+        'baker': {
+            'lambda': 392,  # W/mK
+            'cp': 390,  # J/kgK
         }
     }
 
     if material in materiali:
         return(materiali[material])
     else:
-        raise Exception('Sorry, material ne obstaja') 
-
+        raise Exception('Sorry, material ne obstaja')
