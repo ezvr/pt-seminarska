308237b330de6104e107d9d82b092b4acf85be83
JakaPuric <jakapuric@gmail.com>
2021-04-03 18:09:49 +0200

    menjava lambda za k

--- knjiznicaMaterialov.py
+++ knjiznicaMaterialov.py
@@ -1,25 +1,25 @@
 def knjiznica(material):
     materiali = {
         'PMMA': {
-            'lambda': 0.193,  # W/mK
+            'k': 0.193,  # W/mK
             'cp': 1450,  # J/kgK
 
 
         },
         'ABS': {
-            'lambda': 0.24,  # W/mK
+            'k': 0.24,  # W/mK
             'cp': 1675,  # J/kgK
         },
         'aluminij': {
-            'lambda': 240,  # W/mK
+            'k': 240,  # W/mK
             'cp': 910,  # J/kgK
         },
         'zelezo': {
-            'lambda': 65,  # W/mK
+            'k': 65,  # W/mK
             'cp': 450,  # J/kgK
         },
         'baker': {
-            'lambda': 392,  # W/mK
+            'k': 392,  # W/mK
             'cp': 390,  # J/kgK
         }
     }
